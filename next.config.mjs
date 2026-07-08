07197eaf8ad09ba381f39e80f19a099917639008
ShoppingDogam <shoppingdogam@example.com>
2026-07-08 17:31:43 +0900

    Deploy site with GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig = {
+  output: isGithubPages ? "export" : undefined,
+  basePath: isGithubPages ? "/shoppingdogam" : undefined,
+  assetPrefix: isGithubPages ? "/shoppingdogam/" : undefined,
+  trailingSlash: isGithubPages,
   images: {
+    unoptimized: isGithubPages,
     remotePatterns: [
       { protocol: "https", hostname: "**" }
     ]
